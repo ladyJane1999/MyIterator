c5a6a1aeab8ad3193e5af8e6fc9074b98dd000ef
ladyJane1999 <69354433+ladyJane1999@users.noreply.github.com>
2022-08-16 15:06:50 +0300

    Update Iterator.cs

--- MyIterator/Iterator.cs
+++ MyIterator/Iterator.cs
@@ -7,99 +7,58 @@ using System.Threading.Tasks;
 
 namespace Helpers
 {
-    public static class Helpers
+    public class MyIterator<T> : IEnumerator<T>, IEnumerable<T>
     {
-        public class BaseEnumerator<T> : IEnumerator<T>, IEnumerable<T>
+        public Func<int, bool> Predicate;
+
+        public IEnumerator<T> enumerator;
+        public MyIterator(IEnumerator<T> enumer, Func<int, bool> predicate)
         {
-            public BaseEnumerator( Func<int, bool> predicate)
-            {
-                Predicate = predicate;
-              
-            }
+            Predicate = predicate;
+            enumerator = enumer;
 
-            public BaseEnumerator(IEnumerator<T> enumer)
-            {
-                enumerator = enumer;
-            }
-
-            public BaseEnumerator()
-            {
-            }
-
-            public  Func<int, bool> Predicate;
-            protected virtual IEnumerator<T> enumerator { get; set; }
-
-            protected virtual Func<int, bool> predicate { get; set; }
-
-            protected virtual T current { get; set; }
-
-            public virtual bool MoveNext()
-            {
-                return enumerator.MoveNext();
-            }
-
-            public virtual IEnumerator<T> GetEnumerator()
-            {
-                return enumerator;
-            }
-
-            public virtual T Current { get { return enumerator.Current; } }
-
-            object IEnumerator.Current { get { return enumerator.Current; } }
-
-            public virtual void Reset() { }
-
-            public virtual void Dispose() { }
-
-            IEnumerator<T> IEnumerable<T>.GetEnumerator()
-            {
-                return GetEnumerator();
-            }
-
-            IEnumerator IEnumerable.GetEnumerator()
-            {
-                return enumerator;
-            }
         }
-
-        public class IndexedEnumerator<T> : BaseEnumerator<T>
+        public T Current => enumerator.Current;
+        object IEnumerator.Current => enumerator.Current;
+        void IDisposable.Dispose()
         {
-            public IndexedEnumerator( Func<int, bool> predicate) : base( predicate) { }
-
-            public IndexedEnumerator(IEnumerator<T> enumer) : base(enumer) { }
-
-            public IndexedEnumerator()
+           
+        }
+        public int Index { get; private set; }
+        public int Count { get; private set; }
+        public int SetT(int count)
+        {
+            Count = count;
+            return Count;
+        }
+        public bool MoveNext()
+        {
+            if (Predicate(Index))
             {
-            }
-
-            public int Index { get; private set; }
-
-            public int Count { get; private set; }
-            public T GetT()
-            {
-                return enumerator.Current;
-            }
-            public int SetT(int count)
-            {
-                Count = count;
-                return Count;
-            }
-
-            public override bool MoveNext()
-            {
-                int cnt = 0;
-                if (Predicate(Index))
-                {
-                    cnt++;
-                    Console.WriteLine(enumerator.Current);
-                    Index++;
-                }
-
-                if (cnt > Count)
-                    return false;
+                Console.WriteLine(Index);
+                Index++;
                 return true;
             }
+            Index++;
+            return false;
+        }
 
+        void IEnumerator.Reset()
+        {
+           
+        }
+        public virtual IEnumerator<T> GetEnumerator()
+        {
+            return enumerator;
+        }
+        IEnumerator<T> IEnumerable<T>.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return enumerator;
         }
     }
 }
